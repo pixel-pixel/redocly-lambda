ccbdfbaab2e6513b92a3da5294decac255ef0768
Andrii Bartish <a.bartish@rozetkapay.com>
2025-03-27 12:34:47 +0200

    Add conditions

--- tooling/github-webhook/src/index.ts
+++ tooling/github-webhook/src/index.ts
@@ -1,13 +1,13 @@
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Context) => {
-  console.log('#Lambda works with TS');
+  console.log('#Lambda works');
   
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Lambda works',
         input: event,
       },
       null,
